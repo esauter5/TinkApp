56f4d5c82c268b6c8b8dc69c277032a617711998
Eric Sauter <esauter5@gmail.com>
2013-01-03 22:47:18 -0500

    Send to either of us

--- app/controllers/tinks_controller.rb
+++ app/controllers/tinks_controller.rb
@@ -14,7 +14,16 @@ class TinksController < ApplicationController
   end
 
   def create
-  	@tink = Tink.new(params[:tink])
+    @tink = Tink.new
+    #@tink = Tink.new(params[:tink])
+    @tink.url = params[:tink][:url]
+    @tink.receiver = params[:tink][:receiver]
+
+    if @tink.receiver == 1
+      did = '5B3275894AEB9D7E9693EBD33105B54ECCA8CE4CB7EC11D846B47FCCC0607EEB'
+    else
+      did = 'F421EC8F0AA6D1FE4B9286C04A48168A12512AD9AB669A78DC36C61D708AE2F5'
+    end
 
     title = Nokogiri::HTML(open(@tink.url)).title()
 
@@ -24,7 +33,7 @@ class TinksController < ApplicationController
     u = bitly.shorten(@tink.url)
 
     notification = {
-      :device_tokens => ['5B3275894AEB9D7E9693EBD33105B54ECCA8CE4CB7EC11D846B47FCCC0607EEB'],
+      :device_tokens => [did],
       :aps => {:alert => "#{title} #{u.short_url}", :badge => 1}
     }
 
--- app/models/tink.rb
+++ app/models/tink.rb
@@ -1,4 +1,5 @@
 class Tink < ActiveRecord::Base
   belongs_to :user
   attr_accessible :date_of_send, :title, :url
+  attr_accessor :receiver
 end
