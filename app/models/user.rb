6732dccc9d3638c399f4ca2e4f9dde245a7e48d6
Eric Sauter <esauter5@gmail.com>
2013-01-05 17:16:38 -0500

    Removed sender checkbox

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+lclass User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :device_id
   # attr_accessible :title, :body
 
   has_many :tinks
